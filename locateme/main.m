a7f51d54d358d740eff7ff2ffb5d990c900a14d6
Andy Lindeman <alindeman@gmail.com>
2013-01-27 15:35:30 -0500

    Initial Commit

---
+++ locateme/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  locateme
+//
+//  Created by ADML on 2013-01-27.
+//  Copyright (c) 2013 Andy Lindeman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
